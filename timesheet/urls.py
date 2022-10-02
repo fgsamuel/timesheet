435601b2763e19a0306864735cf7bf421ad8ea30
Samuel <fg.samuel@gmail.com>
2022-10-01 22:05:28 -0300

    create openApi documentation with swagger for api

--- timesheet/urls.py
+++ timesheet/urls.py
@@ -16,10 +16,30 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include
 from django.urls import path
+from django.urls import re_path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 from rest_framework_simplejwt.views import TokenObtainPairView
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Timesheet Backend Service",
+        default_version="v1",
+        description="Backend API to manage the hours employees work on certain projects",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="fg.samuel@gmail.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/authenticate/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/v1/", include("timesheet.core.api.v1.urls")),
+    re_path(r"^swagger(?P<format>\.json|\.yaml)$", schema_view.without_ui(cache_timeout=0), name="schema-json"),
+    re_path(r"^swagger/$", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
+    re_path(r"^redoc/$", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
 ]
